3d1b743ab77b317624ca53f9524a8ac5d8b0e147
Ewerson Martins <ewersonmrt@gmail.com>
2022-10-01 21:18:21 -0300

    feat: Define middleware no back-end de maneira que ele não permita o acesso sem informar um password no front-end

--- app/backend/src/middlewares/auth/login.Auth.ts
+++ app/backend/src/middlewares/auth/login.Auth.ts
@@ -1,9 +1,9 @@
 import { NextFunction, Request, Response } from 'express';
 
 export default function authenticateLogin(req: Request, res: Response, next: NextFunction) {
-  const { email } = req.body;
+  const { email, password } = req.body;
 
-  if (!email) {
+  if (!email || !password) {
     return res.status(400).json({ message: 'All fields must be filled' });
   }
 
