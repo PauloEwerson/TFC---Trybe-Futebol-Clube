300b9b7edc0cedb423caa9a6b34298eb200a0d39
Ewerson Martins <ewersonmrt@gmail.com>
2022-10-03 18:36:31 -0300

    feat: Define o endpoint /matches de forma que não possíbilita inserir uma partida com time que não existe na tabela teams

--- app/backend/src/middlewares/auth/match.Auth.ts
+++ app/backend/src/middlewares/auth/match.Auth.ts
@@ -1,12 +1,26 @@
 import { NextFunction, Request, Response } from 'express';
+import Team from '../../database/models/Team';
 
-export default function authenticateMatch(req: Request, res: Response, next: NextFunction) {
+export function authMatch(req: Request, res: Response, next: NextFunction) {
   const { homeTeam, awayTeam } = req.body;
 
   if (homeTeam === awayTeam) {
     return res.status(401).json({
       message: 'It is not possible to create a match with two equal teams',
     });
+  }
+
+  next();
+}
+
+export async function authTeam(req: Request, res: Response, next: NextFunction) {
+  const { homeTeam, awayTeam } = req.body;
+
+  const isHomeTeamValid = await Team.findByPk(homeTeam);
+  const isAwayTeamValid = await Team.findByPk(awayTeam);
+
+  if (!isHomeTeamValid || !isAwayTeamValid) {
+    return res.status(404).json({ message: 'There is no team with such id!' });
   }
 
   next();
--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,11 +1,16 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
-import authenticateMatch from '../middlewares/auth/match.Auth';
+import { authMatch, authTeam } from '../middlewares/auth/match.Auth';
 
 const routes = Router();
 
 const matchesController = new MatchesController();
 routes.get('/', matchesController.getMatches);
-routes.post('/', authenticateMatch, matchesController.createMatch);
+routes.post(
+  '/',
+  authMatch,
+  authTeam,
+  matchesController.createMatch,
+);
 routes.patch('/:id/finish', matchesController.patchMyMatch);
 export default routes;
