8c9ae825a2e4a962d9fadb8d1656ab078f1b9aaf
Ewerson Martins <ewersonmrt@gmail.com>
2022-10-03 18:09:40 -0300

    feat: Define o endpoint /matches de forma que não possíbilita inserir uma partida com times iguais

--- app/backend/src/routes/matchesRoute.ts
+++ app/backend/src/routes/matchesRoute.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import MatchesController from '../controllers/MatchesController';
+import authenticateMatch from '../middlewares/auth/match.Auth';
 
 const routes = Router();
 
 const matchesController = new MatchesController();
 routes.get('/', matchesController.getMatches);
-routes.post('/', matchesController.createMatch);
+routes.post('/', authenticateMatch, matchesController.createMatch);
 routes.patch('/:id/finish', matchesController.patchMyMatch);
 export default routes;
